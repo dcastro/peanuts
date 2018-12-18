0f9176f4cc519a70fbf8711d22ea158f46b68a45
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2018-12-18 08:36:51 +0000

    feat: geo performance improvements

--- app/geo.js
+++ app/geo.js
@@ -6,45 +6,19 @@ var GeoDataHelper = function () {
     var GeoData = function () { };
 
     /**
-     * Cache of transformed geo file.
-     */
-    var dataCache;
-
-    /**
-     * Url of maxmind json data.
-     */
-    const geoAssetUrl = "/assets/geo/geo-data.json";
-
-    /**
      * Enhance data by lat and lng attributes.
      * @param {*} data
      */
     GeoData.prototype.addLatLng = function (data) {
-        if (!dataCache) {
-            throw new Error('geo data not available yet');
-        }
         for (var i = 0; i < data.length; i++) {
-            if (!dataCache[data[i].targets_geo_city]) {
+            if (!GeoDataMap[data[i].targets_geo_city]) {
                 console.log('No location data for ' + JSON.stringify(data[i]))
                 continue;
             }
-            data[i].lat = dataCache[data[i].targets_geo_city][0];
-            data[i].lng = dataCache[data[i].targets_geo_city][1];
+            data[i].lat = GeoDataMap[data[i].targets_geo_city][0] / 10000.0;
+            data[i].lng = GeoDataMap[data[i].targets_geo_city][1] / 10000.0;
         }
         return data;
-    }
-
-    /**
-     * Load maxmind csv and return promise of it, keyed by location id.
-     * @private
-     */
-    GeoData.prototype.init = async function () {
-        if (dataCache) {
-            return Promise.resolve(dataCache);
-        }
-
-        const response = await fetch(geoAssetUrl);
-        dataCache = await response.json();
     }
 
     return new GeoData();
